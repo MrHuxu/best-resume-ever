fc114f67f52266e131b96824227a5419e52307f3
xhu <xhu@freewheel.tv>
2017-09-22 16:12:59 +0800

    update the career part

--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -48,7 +48,7 @@ export default Vue.component('resume', {
   background: white;
   position: relative;
   width: 21cm;
-  height: 54.8cm;
+  height: 56.4cm;
   display: block;
   page-break-after: auto;
   overflow: hidden;
